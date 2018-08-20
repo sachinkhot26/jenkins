042968ace6d65a9f2dd874a3d9496268efffc9d2
Sachin Khot <sachin@Sachins-Air.blr-office.geekyants.com>
2018-08-20 16:44:16 +0530

    second commit

--- src/sample/Jenkins.java
+++ src/sample/Jenkins.java
@@ -1,9 +1,12 @@
+package sample;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Jenkins {
 
-	public static void main(String[] args) {
+	@Test
+	public void launchBrowser() {
 		
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/driver/chromedriver");
 		WebDriver driver=new ChromeDriver();
